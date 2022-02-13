4db9a36e45c0ebbb31219deedf0cb0713e439e22
Nick Patel <nickpatel.developer@gmail.com>
2022-02-13 00:20:30 -0500

    Initial Commit

---
+++ HelloWorldApp/HelloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppApp.swift
+//  HelloWorldApp
+//
+//  Created by Neerav Patel on 2022-02-13.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
